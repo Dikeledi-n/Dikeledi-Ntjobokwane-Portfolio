118f8424eaf573b5a5bbcb4a08e5ab76d71fadf8
Dikeledi Ntjobokwane <156684913+Dikeledi-n@users.noreply.github.com>
2024-11-15 18:56:57 +0200

    New changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,12 +8,12 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description" style={{ fontWeight: 'bold' }}>
-           A dedicated Front-end Developer
+           A dedicated Full Stack Software Developer
           </p>
           <p className="hero--section-description">
-          I am an Anthrpology Honours graduate from the University of Johannesburg and a soon-to-be graduate of Zaio Coding School Bootcamp, I'm excited to bring my skills in frontend development to a dynamic team.
-          With a strong foundation in HTML, CSS, JavaScript and React. I've honed my expertise in building responsive, user-centric interfaces with React. 
-          I also have knowledge of backend development using Node.js, NPM, Postman, and async/await, I have a solid foundation in building server-side applications
+          I am an Anthropology Honours graduate from the University of Johannesburg and a Zaio Coding School Bootcamp graduate.
+          I have a strong foundation in HTML, CSS, JavaScript and React. I've honed my expertise in building responsive and user-centric interfaces with React. 
+          I also have knowledge of backend development using Node.js, NPM, Postman, and async/await and have a solid foundation in building server-side applications.
           My passion lies in crafting seamless web experiences that delight users. I'm eager to collaborate with like-minded professionals and continue learning and growing as a developer. 
           Explore my portfolio to see how I've applied my skills to real-world projects and let's connect to discuss how I can contribute to your team's success.
           </p>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -4,11 +4,11 @@ export default function HeroSection() {
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Dikeledi</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Frontend Developer</span>
+            <span className="hero--section-title--color">Full Stack Software Developer</span>
           </h1>
           <p className="hero--section-description">
-          As a passionate frontend developer, I bring together code and creativity to craft engaging web experiences that captivate and inspire. 
-          With a strong foundation in HTML, CSS, and JavaScript, I expertly balance functionality and aesthetics to create seamless interactions and stunning visuals.
+          Passionate software developer with hands-on experience in building dynamic, responsive, and user-centric web applications.
+          Equipped with a strong foundation in both development and deployment, and ready to contribute to innovative projects in a professional development environment.
           </p>
         </div>
       <div className="hero--section--img">
